46113605b7aca2e802925c57c2bddc6388fdb353
Paulo Braga <uchoapaulo42@gmail.com>
2024-12-16 13:52:45 -0300

    função inputNum para conseguirmos clicar nos números e css da calculadora atualizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Calculator from './components/Calculator';
+import { Box } from '@mui/system';
 
 function App() {
   return (
+    <div>
+      <Box m={5}/>
     <div className="App">
     <Calculator></Calculator>
     </div>
+    </div>
   );
 }
-
+                                                  
 export default App;
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calculator.css'
 import Container from '@mui/material/Container'
+import { Box } from "@mui/system"
 
 export default function Calculator() {
+    const[num, setNum] = useState(0);
+
+    function inputNum (e) {
+        var input = e.target.value
+        setNum( input );
+    }
     return (
+        <div>
+        <Box m={5}/>
         <Container maxWidth="xs">
         <div className='wrapper'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>,</button>
+            <Box m={12}/>
+            <h1 className='result'>{num}</h1>
             <button>AC</button>
             <button>+/-</button>
             <button>%</button>
-            <button>X-</button>
-            <button>-</button>
-            <button>+</button>
-            <button>=</button>
+            <button className="orange">/</button>
+            <button className="gray" onClick={inputNum} value={7}>7</button>
+            <button className="gray" onClick={inputNum} value={8}>8</button>
+            <button className="gray" onClick={inputNum} value={9}>9</button>
+            <button className="orange">X</button>
+            <button className="gray" onClick={inputNum} value={4}>4</button>
+            <button className="gray" onClick={inputNum} value={5}>5</button>
+            <button className="gray" onClick={inputNum} value={6}>6</button>
+            <button className="orange">-</button>
+            <button className="gray" onClick={inputNum} value={1}>1</button>
+            <button className="gray" onClick={inputNum} value={2}>2</button>
+            <button className="gray" onClick={inputNum} value={3}>3</button>
+            <button className="orange">+</button>
+            <button className="gray" onClick={inputNum} value={0}>0</button>
+            <button className="gray">,</button>
+            <button className="gray" style={{visibility: "hidden"}}>,</button>
+            <button className="orange">=</button>
         </div>
         </Container>
+        </div>
     )
 }
